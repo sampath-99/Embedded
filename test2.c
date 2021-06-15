2cd4b98a9789019b26af0fd1dd50264d9a047ccb
sampath-99 <85913689+sampath-99@users.noreply.github.com>
2021-06-15 19:40:16 +0530

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+#if 0
+	/*factorial program*/
+	int x,y,z,u;
+	x=10;
+	x=x+10;
+	y=y*10;
+	/*to execute anotherway*/
+	z=u*7;
+	printf("x=%d\n",x);
+	printf("y=%d\n",y);
+	printf("z=%d\n",z);
+#endif	
+}
